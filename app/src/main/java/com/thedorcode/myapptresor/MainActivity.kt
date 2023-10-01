4df685c2216f0aec5e4c9412ac433cc068f2ddea
tshimsthedoor <tshims79@gmail.com>
2023-10-01 19:39:54 +0200

    Now we can select and click the button

--- app/src/main/java/com/thedorcode/myapptresor/MainActivity.kt
+++ app/src/main/java/com/thedorcode/myapptresor/MainActivity.kt
@@ -2,10 +2,21 @@ package com.thedorcode.myapptresor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener{
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = "Beer color is $color"
+
+        }
     }
 }
